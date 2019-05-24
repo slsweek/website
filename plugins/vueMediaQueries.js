4fcae60d9fef842848148515f4d08b7b0f7bb0dc
Igor Halfeld <hello@igorluiz.me>
2019-05-23 22:56:42 -0300

    remove device detect

--- plugins/vueMediaQueries.js
+++ plugins/vueMediaQueries.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import json2mq from 'json2mq';
-import MobileDetect from 'mobile-detect';
-
 
 const VueMediaQuery = Vue => {
   if (Vue.prototype.$mq) return;
@@ -14,11 +12,6 @@ const VueMediaQuery = Vue => {
           json2mq(query),
         );
 
-        const md = new MobileDetect(window.navigator.userAgent);
-
-        if (md.mobile()) {
-          return false;
-        }
         return mq.matches;
       }
     },
