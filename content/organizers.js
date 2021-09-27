096f3a11b9ffa9164b5627501d2518e9071469c5
Igor Halfeld <hello@igorluiz.me>
2021-09-27 10:36:30 -0300

    change organizers description

--- content/organizers.js
+++ content/organizers.js
@@ -2,7 +2,7 @@ export default [
   {
     name: 'Igor Halfeld',
     image: 'https://avatars3.githubusercontent.com/u/9022134?s=400&v=4',
-    description: 'Microsoft MVP, Speaker, Senior Software Engineer at Estratégia Concursos, Organizer at @vuejssummit, @vuejs_brasil, @vuejssp, @jssp and @nodebr',
+    description: 'Microsoft MVP, Speaker, Engineering Manager at TC (TradersClub), Organizer at @vuejssummit, @vuejs_brasil, @vuejssp, @jssp and @nodebr',
     twitter: 'igorhalfeld',
     github: 'igorhalfeld',
     linkedin: 'igorhalfeld',
@@ -10,7 +10,7 @@ export default [
   {
     name: 'Lucas Santos',
     image: 'https://i.imgur.com/w7iw81E.png',
-    description: 'Microsoft MVP, Google Developer Expert, Speaker, Senior Solutions Architect at Nindoo.ai, Organizer at @abc_developers, @nodeschoolsp, @vuejssp, @trainingcentr, @jssp and @nodebr',
+    description: 'Microsoft MVP, Google Developer Expert, Speaker, Developer Advocate at Microsoft, Organizer at @abc_developers, @nodeschoolsp, @vuejssp, @trainingcentr, @jssp and @nodebr',
     twitter: '_staticvoid',
     github: 'khaosdoctor',
     linkedin: 'lhs-santos',
